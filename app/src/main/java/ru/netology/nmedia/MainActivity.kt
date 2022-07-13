b0d916f091ae290ee440bff148a5dac04a1ad843
Oleg Sysolyatin <osysolyatin@gmail.com>
2022-07-13 17:04:07 +0400

    homework 1-2

--- app/src/main/java/ru/netology/nmedia/MainActivity.kt
+++ app/src/main/java/ru/netology/nmedia/MainActivity.kt
@@ -19,7 +19,7 @@ class MainActivity : ComponentActivity() {
             NMediaTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
-                    Greeting("NMedia!")
+                    Greeting("NMedia")
                 }
             }
         }
